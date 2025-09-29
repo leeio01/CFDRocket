2c6e30a4c353a0e8644826d43080d51cd753fb19
leeio01 <177035474+leeio01@users.noreply.github.com>
2025-09-29 23:47:26 +0500

    added the backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000';
+
+const API_URL = import.meta.env.VITE_API_URL || 'https://cfdrocket.onrender.com';
 
 const api = axios.create({ baseURL: API_URL });
 
